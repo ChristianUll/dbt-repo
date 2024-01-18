0a22c268ebe003fd3ef678a93361b1317331545a
ChristianUll <christian.ullrich@gmail.com>
2024-01-18 15:40:56 +0100

    ..

---
+++ models/staging/staging_wind.sql
@@ -0,0 +1,12 @@
+WITH wind_daily AS (
+    SELECT ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'date')::VARCHAR)::date  AS date,
+        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxwind_kph')::VARCHAR)::FLOAT AS maxwind_kph,
+        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'wind_degree')::VARCHAR)::FLOAT AS wind_degree,
+        (extracted_data -> 'location' -> 'name')::VARCHAR  AS city,
+        (extracted_data -> 'location' -> 'region')::VARCHAR  AS region,
+        (extracted_data -> 'location' -> 'country')::VARCHAR  AS country
+    FROM {{source("staging", "raw_temp")}})
+SELECT * 
+FROM wind_daily
+
+
