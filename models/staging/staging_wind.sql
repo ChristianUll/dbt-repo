5046cfddbce4c22eb49ec4ba985ab426df721a1a
ChristianUll <christian.ullrich@gmail.com>
2024-01-18 15:23:45 +0100

    ..

--- models/staging/staging_wind.sql
+++ models/staging/staging_wind.sql
@@ -1,6 +1,6 @@
 WITH wind_daily AS (
     SELECT ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'date')::VARCHAR)::date  AS date,
-        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
+        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxwind_kph')::VARCHAR)::FLOAT AS maxwind_kph,
         ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'mintemp_c')::VARCHAR)::FLOAT AS mintemp_c,
         ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avgtemp_c')::VARCHAR)::FLOAT AS avgtemp_c,
         (extracted_data -> 'location' -> 'name')::VARCHAR  AS city,
