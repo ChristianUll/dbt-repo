141e36cebee9652e5a786c6ef2b9f67496fca474
ChristianUll <christian.ullrich@gmail.com>
2024-01-18 13:34:30 +0100

    ..

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -9,9 +9,9 @@ add_weekday AS (
 
     EXTRACT(DOW FROM date) AS weekday,  -- Extracts the day of the week (0-6, where 0 is Sunday)
     EXTRACT(DAY FROM date) AS day_num,    -- Extracts the day of the month
-    EXTRACT(WEEK FROM date) AS week,
-    EXTRACT(MONTH FROM date) AS month,
-    EXTRACT(YEAR FROM date) AS year
+    EXTRACT(WEEK FROM date) AS week_num,
+    EXTRACT(MONTH FROM date) AS month_num,
+    EXTRACT(YEAR FROM date) AS year_num
     FROM temp_daily
 )
 SELECT *
