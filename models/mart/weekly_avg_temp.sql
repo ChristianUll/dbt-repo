219866d9ef2405cd9d555d088f7326444a5b4e3c
ChristianUll <christian.ullrich@gmail.com>
2024-01-18 10:59:39 +0100

    ..

---
+++ models/mart/weekly_avg_temp.sql
@@ -0,0 +1,10 @@
+with total_avg as(
+    select
+    city, country, year, lat, lon,
+    AVG(avgtemp_c) as avg_temp_week
+    MAX(maxtemp_c) as max_temp_week,
+    MIN(mintemp_c) as min_temp_weekday
+    FROM {{ref("prep_temp")}}
+    GROUP BY city, country, year, week, lat, lon
+)
+select * FROM total_avg    
