d2bd97d0a27d545788cb0be645f7b1939b16153e
ChristianUll <christian.ullrich@gmail.com>
2024-01-18 12:16:26 +0100

    ..

---
+++ models/prep/prep_temp.sql
@@ -0,0 +1,15 @@
+WITH --start the chain of CTEs
+
+temp_daily AS (
+    SELECT * 
+    FROM {{ref('staging_temp')}}
+),
+add_weekday AS (
+    SELECT *,
+
+    EXTRACT(DOW FROM date) AS weekday,  -- Extracts the day of the week (0-6, where 0 is Sunday)
+    EXTRACT(DAY FROM date) AS day_num    -- Extracts the day of the month
+    FROM temp_daily
+)
+SELECT *
+FROM add_weekday
